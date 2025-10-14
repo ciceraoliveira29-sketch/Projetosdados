c74a3699b7d7347822c5d29e6b8f08f01deedd23
Maria Cicera <ciceraoliveira29@gmail.com>
2025-10-13 21:15:35 -0300

    Imprimindo até a idade informada

--- OlaMundo.py
+++ OlaMundo.py
@@ -1,2 +1,12 @@
 nome = input('Digite seu nome:')
 print('Ola,'+ nome + '!')
+
+idade = int(input('Digite sua idade:'))
+if (idade >= 18):
+    print('Você e maior de idade.')
+else:
+    print('Você e menor de idade.')
+
+for i in range(1,idade + 1):
+    print(i)
+
