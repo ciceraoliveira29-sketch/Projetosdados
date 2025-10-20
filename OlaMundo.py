98809e95cb0982225c93806fac59694b4bca192e
Maria Cicera <ciceraoliveira29@gmail.com>
2025-10-20 18:55:41 -0300

    listas

--- OlaMundo.py
+++ OlaMundo.py
@@ -1,12 +1,4 @@
-nome = input('Digite seu nome:')
-print('Ola,'+ nome + '!')
-
-idade = int(input('Digite sua idade:'))
-if (idade >= 18):
-    print('Você e maior de idade.')
-else:
-    print('Você e menor de idade.')
-
-for i in range(1,idade + 1):
-    print(i)
+frutas = ['maçã','uva','banana']
+for fruta in frutas:
+    print(frutas)
 
